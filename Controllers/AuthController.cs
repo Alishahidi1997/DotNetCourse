5c44cda2af5841648570f57faed336b8b4a43b1a
Ali Shahidi <shahidi.ali.1997@gmail.com>
2026-03-16 09:49:17 -0600

    jwt

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -107,8 +107,47 @@ namespace DotnetAPI.Controllers
                     return StatusCode(401,"Incorrect Password"); 
                 }
             }
-            return Ok(); 
+            int userId = _dp.LoadDataSingle<int>($"Select UserId From TutorialAppSchema.Users where '{userForLogin.UserName}' = Email"); 
+            return Ok(new Dictionary<string, string>
+            {
+                {"token", CreateToken(userId)}
+            }); 
                 
+        }
+
+        
+        private string CreateToken(int userId)
+        {
+            Claim[] claims = new Claim[] {
+                new Claim("userId", userId.ToString())
+            };
+
+            string? tokenKeyString = _config.GetSection("AppSettings:TokenKey").Value;
+
+            SymmetricSecurityKey tokenKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(
+                        tokenKeyString != null ? tokenKeyString : ""
+                    )
+                );
+
+            SigningCredentials credentials = new SigningCredentials(
+                    tokenKey,
+                    SecurityAlgorithms.HmacSha512Signature
+                );
+
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = credentials,
+                Expires = DateTime.Now.AddDays(1)
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            SecurityToken token = tokenHandler.CreateToken(descriptor);
+
+            return tokenHandler.WriteToken(token);
+
         }
     }
 }
